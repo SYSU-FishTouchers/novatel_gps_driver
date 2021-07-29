86ef61efcf3cba06a23ecc4b4964d29ab84377cf
PanJie <3028481711@qq.com>
2021-07-29 10:27:36 +0800

    代码优化

--- serial_port/src/serial_port.cpp
+++ serial_port/src/serial_port.cpp
@@ -21,13 +21,13 @@ bool get_data_body(const string &data, vector<string> *);
 
 serial_port::Range get_range_msg(const vector<string> &, int);
 
-sensor_msgs::Imu get_imu_msg(const vector<string> &, int);
+sensor_msgs::Imu get_imu_msg(const vector<string> &, int, string);
 
 serial_port::BDSephem get_ephem_msg(const vector<string> &, int);
 
 serial_port::Gprmc get_gprmc_msg(const vector<string> &, int);
 
-vector<string> switch_time_zone(vector<string> gprmc_body,int time_zone);
+vector<string> time_correct(vector<string> gprmc_body, int time_zone);
 
 /*
  * 写在最前：
@@ -122,8 +122,8 @@ int main(int argc, char **argv) {
     // 创建句柄
     ros::NodeHandle n;
     // publisher
-    ros::Publisher range_pub = n.advertise<serial_port::Range>("range", 1000);
     ros::Publisher imu_pub = n.advertise<sensor_msgs::Imu>("imu", 1000);
+    ros::Publisher range_pub = n.advertise<serial_port::Range>("range", 1000);
     ros::Publisher ephem_pub = n.advertise<serial_port::BDSephem>("ephem", 1000);
     ros::Publisher gprmc_pub = n.advertise<serial_port::Gprmc>("gprmc", 1000);
     // 默认参数
@@ -134,7 +134,7 @@ int main(int argc, char **argv) {
     float range_frequency;  //　s/次
     string ephem_frequency = "onchanged";
     float gprmc_frequency;  //　s/次
-    int time_zone;
+    int time_zone;          //  -12 ~ 12
 
     // 从参数服务器获取值
     // 串口参数
@@ -142,12 +142,17 @@ int main(int argc, char **argv) {
     n.param("/serial_port/serial_baudrate", serial_baudrate, 115200);
     // 接收机参数
     n.param("imu_frame_id_param", imu_frame_id_param, std::string("gnss"));
-    n.param("/serial_port/time_zone", time_zone, 0);
     n.param("/serial_port/imu_frequency", imu_frequency, 0.01f);
     n.param("/serial_port/range_frequency", range_frequency, 0.05f);
     n.param("/serial_port/ephem_frequency", ephem_frequency);
     n.param("/serial_port/gprmc_frequency", gprmc_frequency, 1.f);
+    n.param("/serial_port/time_zone", time_zone, 0);
 
+    if ((time_zone > 12) || (time_zone < (-12))) {
+        cout << endl;
+        ROS_ERROR("Time zone setting error, please reset!\n");
+        return 1;
+    }
 
     // 新建一个 GNSS 实例：以后改进成单例
     GNSS gnss(serial_port, serial_baudrate);
@@ -155,7 +160,7 @@ int main(int argc, char **argv) {
         return -1;
     }
     // 原始数据的发送频率
-    std::string imu_command, range_command, ephem_command,gprmc_command;
+    std::string imu_command, range_command, ephem_command, gprmc_command;
     imu_command = std::string("log rawimua ontime ") + std::to_string(imu_frequency);
     range_command = std::string("log rangea ontime ") + std::to_string(range_frequency);
     ephem_command = std::string("log bdsephemerisa ") + ephem_frequency;
@@ -172,31 +177,23 @@ int main(int argc, char **argv) {
     while (ros::ok()) {
         std::string data = gnss.read_line();
         if (data.empty()) continue;
-        //ROS_INFO("\ndata = %sdata.size() = %lu \n" ,data.c_str(), data.size());
+
         if (check_msg(data) == "RANGEA") {
             // 将数据存进 range_body
             vector<string> range_body;
             bool check = get_data_body(data, &range_body);
             if (!check) continue;
-            /* debug /
-            ROS_ERROR("debug range_body");
-            for(auto c : range_body)
-                cout << c << " ";
-            cout << endl;
-            ROS_ERROR("========debug fields=======");
-            / debug */
             // 发布 range_msg
             serial_port::Range range_msg = get_range_msg(range_body, count_r);
             range_pub.publish(range_msg);
             count_r++;
-        }
-        else if (check_msg(data) == "RAWIMUA") {
+        } else if (check_msg(data) == "RAWIMUA") {
             // 将数据存进 imu_body
             vector<string> imu_body;
             bool check = get_data_body(data, &imu_body);
             if (!check) continue;
             // 发布 range_msg
-            sensor_msgs::Imu imu_msg = get_imu_msg(imu_body, count_i);
+            sensor_msgs::Imu imu_msg = get_imu_msg(imu_body, count_i, imu_frame_id_param);
             imu_pub.publish(imu_msg);
             count_i++;
         } else if (check_msg(data) == "BDSEPHEMERISA") {
@@ -204,13 +201,6 @@ int main(int argc, char **argv) {
             vector<string> ephem_body;
             bool check = get_data_body(data, &ephem_body);
             if (!check) continue;
-            /* debug /
-            ROS_ERROR("debug ephem_body");
-            for(auto c : ephem_body)
-                cout << c << " ";
-            cout << endl;
-            ROS_ERROR("========debug ephem=======");
-            / debug */
             // 发布 ephem_msg
             serial_port::BDSephem ephem_msg = get_ephem_msg(ephem_body, count_e);
             ephem_pub.publish(ephem_msg);
@@ -218,26 +208,20 @@ int main(int argc, char **argv) {
         } else if (check_msg(data) == string("GPRMC")) {
             // 将数据存进 ephem_body
             vector<string> gprmc_body;
-            bool check = get_data_body(data,&gprmc_body);
-            if(!check) continue;
-            // 时区转换
+            bool check = get_data_body(data, &gprmc_body);
+            if (!check) continue;
+            // 时区时间校正
             if (time_zone) {
-                gprmc_body = switch_time_zone(gprmc_body, time_zone);
+                gprmc_body = time_correct(gprmc_body, time_zone);
             }
-            /* debug /
-            ROS_ERROR("debug gprmc_body");
-            for(auto c : gprmc_body)
-                cout << c << " ";
-            cout << endl;
-            ROS_ERROR("========debug gprmc=======");
-            / debug */
             // 发布 gprmc_msg
             serial_port::Gprmc gprmc_msg = get_gprmc_msg(gprmc_body, count_g);
             gprmc_pub.publish(gprmc_msg);
             count_g++;
         } else if (check_msg(data) == string("Empty data")) {
             continue;
-        } else ROS_WARN("Unkonwn data, will not be processed: %s", data.c_str());
+        } else
+            ROS_WARN("Unkonwn data, will not be processed: %s", data.c_str());
 
         loop_rate.sleep();
     }
@@ -250,7 +234,7 @@ string check_msg(const string &data) {
     string check = "Empty data";
     if (data.size() > 70) {
         //find 返回所查找字符的下标，当查找的字符不存在时返回 -1 ，而返回 0 时说明字符存在且下标为1
-        if(data.find('#')+1){
+        if (data.find('#') + 1) {
             check = data.substr(data.find('#') + 1, data.find(',') - data.find('#') - 1);
         } else if (data.find('$') + 1) {
             check = data.substr(data.find('$') + 1, data.find(',') - data.find('$') - 1);
@@ -263,7 +247,7 @@ string check_msg(const string &data) {
 bool get_data_body(const string &data, vector<string> *fields) {
     // 获取数据 body
     int begin = data.find(';'), end = data.rfind('*');
-    string content = data.substr(begin + 1, end - begin - 1)+string(",*EndID"); //添加末尾标识符
+    string content = data.substr(begin + 1, end - begin - 1) + string(",*EndID"); //添加末尾标识符
     // 以逗号为区分，将各个字段的数据存进 fields
     if (data.size() > 70) {
         int next_comma;
@@ -271,14 +255,13 @@ bool get_data_body(const string &data, vector<string> *fields) {
             next_comma = content.find(',');
             fields->emplace_back(content.substr(0, next_comma));
             content = content.substr(next_comma + 1);
-            if(content == "*EndID") break;
+            if (content == "*EndID") break;
         }
-    }
-    else {
+    } else {
         string header = check_msg(data);
-        for( auto &c : header)
+        for (auto &c : header)
             c = tolower(c);
-        ROS_WARN("false get %s_body",header.c_str());
+        ROS_WARN("false get %s_body", header.c_str());
         return false;
     }
 
@@ -300,7 +283,6 @@ serial_port::Range get_range_msg(const vector<string> &range_body, int count) {
     init.locktime = 0.0;
     init.tracking_status = 0;
     int32_t numb_of_observ = strtol(range_body[0].c_str(), nullptr, 10);
-//    vector<serial_port::RangeInformation> tmp(numb_of_observ, init);
 
     //声明range_msg
     serial_port::Range range_msg;
@@ -324,25 +306,17 @@ serial_port::Range get_range_msg(const vector<string> &range_body, int count) {
         range_msg.info[j].locktime = strtof(range_body[i + 9].c_str(), nullptr);
         range_msg.info[j].tracking_status = strtol(range_body[i + 10].c_str(), nullptr, 10);
     }
-    /* debug /
-    ROS_WARN("tag:range_msg.info");
-    for(auto i : range_msg.info)
-        cout << i << " ";
-    cout << endl;
-    ROS_WARN("==============================split line============================");
-     / debug */
 
     return range_msg;
-
 }
 
 // 将 imu 数据转换成 ros 消息
-sensor_msgs::Imu get_imu_msg(const vector<string> &imu_body, int count) {
+sensor_msgs::Imu get_imu_msg(const vector<string> &imu_body, int count, string imu_frame) {
     sensor_msgs::Imu imu_msg;
     //header
     imu_msg.header.seq = count;
     imu_msg.header.stamp = ros::Time::now();
-    imu_msg.header.frame_id = "imu_frame";
+    imu_msg.header.frame_id = imu_frame;
     //acc
     imu_msg.linear_acceleration.x = strtof(imu_body[5].c_str(), nullptr) * (0.200 / 65536) / 125;
     imu_msg.linear_acceleration.y = strtof(imu_body[4].c_str(), nullptr) * (0.200 / 65536) / 125;
@@ -399,88 +373,92 @@ serial_port::Gprmc get_gprmc_msg(const vector<string> &grpmc_body, int count) {
     serial_port::Gprmc gprmc_msg;
     gprmc_msg.utc = grpmc_body[1];
     gprmc_msg.pos_status = grpmc_body[2];
-    gprmc_msg.latitude = strtof(grpmc_body[3].c_str(),nullptr);
+    gprmc_msg.latitude = strtof(grpmc_body[3].c_str(), nullptr);
     gprmc_msg.lat_dir = grpmc_body[4];
-    gprmc_msg.longitude = strtof(grpmc_body[5].c_str(),nullptr);
+    gprmc_msg.longitude = strtof(grpmc_body[5].c_str(), nullptr);
     gprmc_msg.lon_dir = grpmc_body[6];
-    gprmc_msg.speed = strtof(grpmc_body[7].c_str(),nullptr);
-    gprmc_msg.track = strtof(grpmc_body[8].c_str(),nullptr);
+    gprmc_msg.speed = strtof(grpmc_body[7].c_str(), nullptr);
+    gprmc_msg.track = strtof(grpmc_body[8].c_str(), nullptr);
     gprmc_msg.date = grpmc_body[9];
-    gprmc_msg.mag_var = strtof(grpmc_body[10].c_str(),nullptr);
+    gprmc_msg.mag_var = strtof(grpmc_body[10].c_str(), nullptr);
     gprmc_msg.var_dir = grpmc_body[11];
     gprmc_msg.mode_ind = grpmc_body[12];
 
     return gprmc_msg;
 }
 
-vector<string> switch_time_zone(vector<string> gprmc_body,int time_zone){
+// 时区时间校正
+vector<string> time_correct(vector<string> gprmc_body, int time_zone) {
     // 获取当前时间
     float time = strtof(gprmc_body[1].c_str(), nullptr);
     // 世界标准时加上时区时差
     time += float(time_zone * 10000);
 
-    /// 时间溢出处理
-    // 每个月的天数
-    vector<uint> day_num = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-    // 获取当前年月日
-    int year = strtol(gprmc_body[9].substr(4,2).c_str(), nullptr, 10);
-    int month = strtol(gprmc_body[9].substr(2, 2).c_str(), nullptr, 10);
-    int day = strtol(gprmc_body[9].substr(0, 2).c_str(), nullptr, 10);
-    // 闰年的 2 月有 29天
-    if (year % 4 == 0){
-        day_num[2] = 29;
+    // 时间溢出处理
+    if (time > 240000 || time < 0) {
+        // 每个月的天数
+        vector<uint> day_num = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        // 获取当前年月日
+        int year = strtol(gprmc_body[9].substr(4, 2).c_str(), nullptr, 10);
+        int month = strtol(gprmc_body[9].substr(2, 2).c_str(), nullptr, 10);
+        int day = strtol(gprmc_body[9].substr(0, 2).c_str(), nullptr, 10);
+        // 闰年的 2 月有 29 天
+        if (year % 4 == 0) {
+            day_num[2] = 29;
+        }
+        // hour 溢出处理
+        if (time > 240000.0) {
+            time -= float(240000.0);
+            day += 1;
+        } else if (time < 0) {
+            time += float(240000.0);
+            day -= 1;
+        }
+        // day 溢出处理
+        if (day > day_num[month]) {
+            day = 1;
+            month += 1;
+        } else if (day <= 0) {
+            day = day_num[month - 1];
+            month -= 1;
+        }
+        // month 溢出处理
+        if (month > 12) {
+            month = 1;
+            year += 1;
+        } else if (month <= 0) {
+            month = 12;
+            year -= 1;
+        }
+        // year 溢出处理
+        if (year == 100) {
+            year = 0;
+        } else if (year < 0) {
+            year = 99;
+        }
+        // 给日期成员赋予新值
+        string day_str = std::to_string(day);
+        string month_str = std::to_string(month);
+        string year_str = std::to_string(year);
+        if (day_str.size() < 2) {
+            day_str = string("0") + day_str;
+        }
+        if (month_str.size() < 2) {
+            month_str = string("0") + month_str;
+        }
+        if (year_str.size() < 2) {
+            year_str = string("0") + year_str;
+        }
+        gprmc_body[9] = day_str + month_str + year_str;
     }
 
-    // hour 溢出处理
-    if (time > 240000.0){
-        time -= float(240000.0);
-        day += 1;
-    } else if(time < 0) {
-        time += float(240000.0);
-        day -= 1;
-    }
-    // day 溢出处理
-    if (day > day_num[month]){
-        day = 1;
-        month += 1;
-    } else if(day <= 0) {
-        day = day_num[month-1];
-        month -= 1;
-    }
-    // month 溢出处理
-    if (month > 12) {
-        month = 1;
-        year += 1;
-    } else if (month <= 0){
-        month = 12;
-        year -= 1;
-    }
-    // year 溢出处理
-    if (year >= 100) {
-        year = 1;
-    } else if (year < 0) {
-        year = 99;
-    }
+    // 给时间成员赋予新值
     string time_str = std::to_string(time);
-    size_t time_lenth = time_str.substr(0,time_str.find('.')).size();
-    if(time_lenth < 6) {
+    size_t time_lenth = time_str.substr(0, time_str.find('.')).size();
+    if (time_lenth < 6) {
         time_str = string("0") + time_str;
     }
     gprmc_body[1] = time_str;
-
-    string day_str = std::to_string(day);
-    string month_str = std::to_string(month);
-    string year_str = std::to_string(year);
-    if (day_str.size() < 2) {
-        day_str = string("0") + day_str;
-    }
-    if (month_str.size() < 2) {
-        month_str = string("0") + month_str;
-    }
-    if (year_str.size() < 2) {
-        year_str = string("0") + year_str;
-    }
-    gprmc_body[9] = day_str + month_str + year_str;
 
     return gprmc_body;
 }
